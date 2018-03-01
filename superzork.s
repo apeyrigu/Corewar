a419365cdc2ab5781f9c409b2a79394993622584
Pierre CLUCHET <pcluchet@e1r5p7.42.fr>
2018-03-01 04:13:29 +0100

    bat zork

--- superzork.s
+++ superzork.s
@@ -1,23 +1,42 @@
-.name "zork"
+.name "SUP"
 .comment "I'M ALIIIIVE"
-
-l1:		ld %191365376, r2
-		st r2, 250
-		ld %268435462, r2
-		st r2, 242
-		ld %1677787136, r2
-		st r2, 234
-		ld %257, r2
-		st r2, 226
-		ld %1, r2
-		st r2, 218
-		ld %167770880, r2
-		st r2, 210
-
-fork:   fork %185
+		
 
 l2:		sti r1, %:live, %1
-		and r1, %0, r1
+l2:		sti r1, %:live1, %1
+l2:		sti r1, %:live2, %1
+l2:		sti r1, %:live3, %1
+l2:		sti r1, %:live4, %1
+l2:		sti r1, %:live5, %1
+l2:		sti r1, %:live6, %1
+l2:		sti r1, %:live7, %1
+l2:		sti r1, %:live8, %1
+l2:		sti r1, %:live9, %1
+l2:		sti r1, %:live10, %1
+l2:		sti r1, %:live11, %1
+l2:		sti r1, %:live12, %1
+l2:		sti r1, %:live13, %1
+		and r1, %0, r2
+
+
 
 live:	live %1
+		fork %:live2
 		zjmp %:live
+
+live1:	live %1
+live2:	live %1
+live3:	live %1
+live4:	live %1
+live5:	live %1
+live6:	live %1
+live7:	live %1
+live8:	live %1
+live9:	live %1
+live10:	live %1
+live11:	live %1
+live12:	live %1
+live13:	live %1
+
+		zjmp %:live1
+
